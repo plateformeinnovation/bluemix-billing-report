d9972f5aa9ce9db617445cef030fb9438ed77540
PnPie <yu.liu003@gmail.com>
2017-02-02 12:12:32 +0100

    change to multi process from multi-thread

--- run.py
+++ run.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 import logging
-import threading
+import multiprocessing
 import time
 
-# register bx_report in sys.modules
 from bx_report import VCAP, bx_login, bx_pw, sleep_time, PORT, app
 from bx_report.DIfactory.get_loader import get_loader
 
 
-class LoadingThread(threading.Thread):
+class LoadingProcess(multiprocessing.Process):
     '''
     The thread to load consumption info from bx tool into PostgreSQL
     '''
 
     def __init__(self, VCAP, bx_login, bx_pw, sleep_time):
         # super object is a proxy object which delegates the methods of LoadingThread's super class
-        super(LoadingThread, self).__init__()
+        super(LoadingProcess, self).__init__()
 
         self.logger = logging.getLogger(__name__)
 
@@ -26,13 +25,16 @@ class LoadingThread(threading.Thread):
     def run(self):
         self.bluemix_loader.load_all_region(self.bluemix_loader.beginning_date)
         while True:
-            self.logger.info('laoding thread sleeping...')
+            self.logger.info('loading process sleeping...')
             time.sleep(sleep_time)
             self.bluemix_loader.load_all_region(self.bluemix_loader.last_month_date())
 
 
-loadingThread = LoadingThread(VCAP, bx_login, bx_pw, sleep_time)
-loadingThread.setDaemon(True)
-loadingThread.start()
+logging.info('CPU cores number: {}'.format(multiprocessing.cpu_count()))
+loadingProcess = LoadingProcess(VCAP, bx_login, bx_pw, sleep_time)
+loadingProcess.daemon = True
+loadingProcess.start()
+logging.info('main process on core: {}'.format(multiprocessing.current_process().ident))
+logging.info('loading child process on core: {}'.format(loadingProcess.ident))
 
-app.run('0.0.0.0', PORT, threaded=True)
+app.run('0.0.0.0', PORT)
